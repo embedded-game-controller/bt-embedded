0100902ce4895b64fe3e07d3ee6197acd589ecfe
Alberto Mardegan <mardy@users.sourceforge.net>
2025-11-11 22:57:15 +0300

    example: test periodic inquiry

--- example/main.c
+++ example/main.c
@@ -9,6 +9,11 @@
 #include "bt-embedded/client.h"
 #include "bt-embedded/hci.h"
 
+static void inquiry_status_cb(BteHci *hci, const BteHciReply *reply, void *)
+{
+    printf("Inquiry issued, status = %d\n", reply->status);
+}
+
 static void inquiry_cb(BteHci *hci, const BteHciInquiryReply *reply, void *)
 {
     printf("Inquiry done, status = %d\n", reply->status);
@@ -19,7 +24,6 @@ static void inquiry_cb(BteHci *hci, const BteHciInquiryReply *reply, void *)
         const uint8_t *b = reply->responses[i].address.bytes;
         printf(" - %02x:%02x:%02x:%02x:%02x:%02x\n", b[0], b[1], b[2], b[3], b[4], b[5]);
     }
-    bte_hci_inquiry(hci, BTE_LAP_GIAC, 3, 0, inquiry_cb);
 }
 
 static void initialized_cb(BteHci *hci, bool success, void *)
@@ -28,7 +32,8 @@ static void initialized_cb(BteHci *hci, bool success, void *)
     printf("ACL MTU=%d, max packets=%d\n",
            bte_hci_get_acl_mtu(hci),
            bte_hci_get_acl_max_packets(hci));
-    bte_hci_inquiry(hci, BTE_LAP_GIAC, 3, 0, inquiry_cb);
+    //bte_hci_inquiry(hci, BTE_LAP_GIAC, 3, 0, inquiry_status_cb, inquiry_cb);
+    bte_hci_periodic_inquiry(hci, 4, 5, BTE_LAP_GIAC, 3, 0, inquiry_status_cb, inquiry_cb);
 }
 
 int main(int argc, char **argv)
