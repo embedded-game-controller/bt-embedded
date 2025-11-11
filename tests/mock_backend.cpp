423a2cc5585a3a090de8ad1cd5cd0ee85617147d
Alberto Mardegan <mardy@users.sourceforge.net>
2025-11-11 23:03:41 +0300

    tests: start writing unit tests

---
+++ tests/mock_backend.cpp
@@ -0,0 +1,144 @@
+#include "mock_backend.h"
+
+#include "bt-embedded/backend.h"
+#include "bt-embedded/internals.h"
+
+#include <cassert>
+#include <errno.h>
+
+MockBackend *MockBackend::s_instance = nullptr;
+
+Buffer::Buffer(BteBuffer *buffer)
+{
+    resize(buffer->total_size);
+
+    BteBufferReader reader;
+    bte_buffer_reader_init(&reader, buffer);
+    uint16_t read = bte_buffer_reader_read(&reader, data(), size());
+    assert(read == size());
+}
+
+static void buffer_free(BteBuffer *buffer)
+{
+    if (buffer->next) buffer_free(buffer->next);
+    delete [] reinterpret_cast<uint8_t*>(buffer);
+}
+
+BteBuffer *Buffer::toBuffer(uint16_t max_packet_size) const
+{
+    if (max_packet_size == 0) max_packet_size = size();
+
+    uint16_t allocated = 0;
+    BteBuffer *buffer = nullptr, *prev = nullptr;
+    while (allocated < size()) {
+        uint16_t packet_size = max_packet_size;
+        if (allocated + packet_size > size()) {
+            packet_size = size() - allocated;
+        }
+        BteBuffer *b = (BteBuffer*)new uint8_t[sizeof(BteBuffer) + packet_size];
+        b->ref_count = 1;
+        b->free_func = buffer_free;
+        b->total_size = size();
+        b->size = packet_size;
+        b->next = nullptr;
+        memcpy(b->data, data() + allocated, packet_size);
+        if (prev) prev->next = b;
+        else buffer = b;
+        prev = b;
+        allocated += packet_size;
+    }
+
+    BteBufferWriter writer;
+    bte_buffer_writer_init(&writer, buffer);
+    bte_buffer_writer_write(&writer, data(), size());
+    return buffer;
+}
+
+MockBackend::MockBackend()
+{
+    s_instance = this;
+}
+
+MockBackend::~MockBackend()
+{
+    s_instance = nullptr;
+}
+
+int MockBackend::callInit()
+{
+    return m_initCb ? m_initCb() : 0;
+}
+
+int MockBackend::callSendCommand(BteBuffer *buffer)
+{
+    m_sentCommands.emplace_back(buffer);
+    if (m_sendCommandCb) {
+        return m_sendCommandCb(buffer);
+    }
+    return 0;
+}
+
+int MockBackend::callSendData(BteBuffer *buffer)
+{
+    m_sentData.emplace_back(buffer);
+    if (m_sendDataCb) {
+        return m_sendDataCb(buffer);
+    }
+    return 0;
+}
+
+int MockBackend::sendQueuedBuffers()
+{
+    int count = m_queuedEvents.size() + m_queuedData.size();
+    for (const Buffer &b: m_queuedEvents) {
+        BteBuffer *buffer = b.toBuffer();
+        _bte_hci_dev_handle_event(buffer);
+        bte_buffer_unref(buffer);
+    }
+    for (const Buffer &b: m_queuedData) {
+        BteBuffer *buffer = b.toBuffer();
+        _bte_hci_dev_handle_data(buffer);
+        bte_buffer_unref(buffer);
+    }
+    return count;
+}
+
+static int mock_init()
+{
+    MockBackend *backend = MockBackend::instance();
+    return backend->callInit();
+}
+
+static int mock_handle_events(bool wait_for_events)
+{
+    MockBackend *backend = MockBackend::instance();
+    return backend->sendQueuedBuffers();
+}
+
+static int mock_hci_send_command(BteBuffer *buffer)
+{
+    MockBackend *backend = MockBackend::instance();
+    return backend->callSendCommand(buffer);
+}
+
+static int mock_hci_send_data(BteBuffer *buffer)
+{
+    MockBackend *backend = MockBackend::instance();
+    return backend->callSendData(buffer);
+}
+
+static int mock_deinit()
+{
+    return 0;
+}
+
+const BteBackend _bte_backend = {
+    .init = mock_init,
+
+    .handle_events = mock_handle_events,
+
+    .hci_send_command = mock_hci_send_command,
+    .hci_send_data = mock_hci_send_data,
+
+    .deinit = mock_deinit,
+};
---
+++ tests/mock_backend.h
@@ -0,0 +1,71 @@
+#ifndef BTE_MOCK_BACKEND_H
+#define BTE_MOCK_BACKEND_H
+
+#include "bt-embedded/backend.h"
+
+#include <functional>
+#include <vector>
+
+class Buffer: public std::vector<uint8_t> {
+public:
+    using std::vector<uint8_t>::vector;
+    Buffer(BteBuffer *buffer);
+
+    BteBuffer *toBuffer(uint16_t max_packet_size = 0) const;
+};
+
+class MockBackend {
+public:
+    MockBackend();
+    ~MockBackend();
+
+    using InitCb = std::function<int()>;
+    void onInit(const InitCb &initBb) {
+        m_initCb = initBb;
+    }
+
+    using SendCb = std::function<int(BteBuffer*)>;
+    void onSendCommand(const SendCb &sendCommandCb) {
+        m_sendCommandCb = sendCommandCb;
+    }
+    void onSendData(const SendCb &sendDataCb) {
+        m_sendDataCb = sendDataCb;
+    }
+
+    const std::vector<Buffer> sentCommands() const { return m_sentCommands; }
+    Buffer lastCommand() const {
+        return m_sentCommands.empty() ? Buffer() : m_sentCommands.back();
+    }
+
+    const std::vector<Buffer> sentData() const { return m_sentData; }
+    Buffer lastData() const {
+        return m_sentData.empty() ? Buffer() : m_sentData.back();
+    }
+
+    void sendEvent(const Buffer &buffer) {
+        m_queuedEvents.push_back(buffer);
+    }
+
+    void sendData(const Buffer &buffer) {
+        m_queuedEvents.push_back(buffer);
+    }
+
+    static MockBackend *instance() { return s_instance; }
+
+    int callInit();
+    int callSendCommand(BteBuffer *buffer);
+    int callSendData(BteBuffer *buffer);
+    int sendQueuedBuffers();
+
+private:
+    static MockBackend *s_instance;
+    InitCb m_initCb;
+    SendCb m_sendCommandCb;
+    SendCb m_sendDataCb;
+    std::vector<Buffer> m_sentCommands;
+    std::vector<Buffer> m_sentData;
+    std::vector<Buffer> m_queuedEvents;
+    std::vector<Buffer> m_queuedData;
+};
+
+#endif /* BTE_MOCK_BACKEND_H */
